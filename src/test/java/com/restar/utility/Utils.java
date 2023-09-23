c3e5ff10b1ac156ed1f68f1e70f834956253d9be
nehadhobale <neha9dhobale@gmail.com>
2023-09-23 19:32:02 +0530

    Issue92001 changes

--- src/test/java/com/restar/utility/Utils.java
+++ src/test/java/com/restar/utility/Utils.java
@@ -1,9 +1,10 @@
 package com.restar.utility;
 
-import io.netty.handler.codec.json.JsonObjectDecoder;
+
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Base64;
 import java.util.Properties;
@@ -11,24 +12,32 @@ import java.util.Random;
 
 public class Utils {
 
-    public static Properties getProperties(String name) throws IOException {
-        File f1 = new File("src//test//resources//" +name+ "Data.properties");
-        FileInputStream fs = new FileInputStream(f1);
-        Properties pro = new Properties();
-        pro.load(fs);
-        return pro;
+    public static Properties getProperties(String name) {
+        try {
+            File f1 = new File("src//test//resources//" + name + "Data.properties");
+            FileInputStream fs = new FileInputStream(f1);
+
+            Properties properties = new Properties();
+            properties.load(fs);
+            return properties;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
-    public static String decode(String encodedStr){
+    public static String decode(String encodedStr) {
         byte[] decodedBytes = Base64.getDecoder().decode(encodedStr);
         String decodedString = new String(decodedBytes);
-        return  decodedString;
+        return decodedString;
     }
 
-    public static String generateIssueNumber(){
+    public static String generateIssueNumber() {
         Random rand = new Random();
-        int max=99999,min=9999;
-       return String.valueOf(rand.nextInt(max-min+1+min));
+        int max = 99999, min = 9999;
+        return String.valueOf(rand.nextInt(max - min + 1 + min));
 
     }
 
